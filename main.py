dac8c9b60dde3a5c7b36a9542a0d7de54a2ccd10
zhengjuncheng <zhengjuncheng@dm-ai.com>
2024-03-25 14:49:12 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+from urllib import request
+
+import numpy
+import requests
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.num
+
+
+
+
